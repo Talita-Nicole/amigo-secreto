0fd435db86e120ffd50b9c7d9d7103032e1a0fc6
Talita Nicole <talitanicoleduarte@gmail.com>
2025-03-16 22:03:34 -0300

    Adicionando arquivos

--- app.js
+++ app.js
@@ -1,17 +1,68 @@
-listaNomes = [];
-
-console.log("teste");
+let listaNomes = [];
 
 function adicionarAmigo(){
     nome = document.querySelector('input');
-    listaNomes.push(nome.value);
-
-    nome.innerHTML = '';
+    if(validarEntrada(nome.value)){
+        listaNomes.push(nome.value);
+    }
+    limparCampo();
+    exibirLista(listaNomes);
 }
 
 
 function sortearAmigo(){
-    let indiceAmigoSorteado = Math.floor(Math.random()*listaNomes.length);
-    console.log(listaNomes[indiceAmigoSorteado]);
+    if(validarSorteio(listaNomes)){
+        let indiceAmigoSorteado = Math.floor(Math.random()*listaNomes.length);
+        let amigoSorteado = listaNomes[indiceAmigoSorteado];
+        exibirResultado(amigoSorteado);
+    }
     
 }
+
+function exibirLista(lista = []){
+    let campo = document.querySelector('#listaAmigos');
+    campo.innerHTML = '';
+    lista.forEach(teste);
+}
+
+function exibirResultado(resultado){
+    let campo = document.querySelector('#resultado');
+    campo.innerHTML = resultado;
+}
+
+function validarEntrada(valor){
+    if(valor == ''){
+        retornarErro('Informe um nome')
+        return 0;
+    }else if(listaNomes.includes(valor)){
+        retornarErro(`O nome ${valor} já foi informado`)
+        return 0;
+    }else{
+        retornarErro('')
+        return 1;
+    }
+}
+
+function validarSorteio(lista = []){
+    if(lista.length < 2){
+        retornarErro('Informe pelo menos 2 nomes');
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function retornarErro(mensagem = ''){
+    let mensagemErro = document.querySelector('p');
+    mensagemErro.innerHTML = mensagem;
+}
+
+function teste(valor){
+    let campo = document.querySelector('#listaAmigos');
+    campo.innerHTML += ' ' + valor + '<br>';
+}
+
+function limparCampo(){
+    nome.value = '';
+}
+
